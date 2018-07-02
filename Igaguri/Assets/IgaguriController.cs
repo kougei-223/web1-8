c01f33a78e65a8a2ee0cbb630d57bcee62b3e9c5
kougei-223 <1727050@st.t-ac.jp>
2018-07-02 17:52:44 +0900

    二回目のコミット

---
+++ Igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour {
+    public void shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+	// Use this for initialization
+	void Start () {
+        // shoot(new Vector3(0, 200, 2000));
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
